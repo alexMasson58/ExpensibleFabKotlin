5c96a8bec855eadf74b24a86d9bf9d78f4cdbc5f
florent champigny <florent.champigny@backelite.com>
2018-05-17 13:43:31 +0200

    added Flo implementation

--- app/src/main/java/com/alexmasson58/expensiblefabexample/MainActivity.kt
+++ app/src/main/java/com/alexmasson58/expensiblefabexample/MainActivity.kt
@@ -1,45 +1,37 @@
 package com.alexmasson58.expensiblefabexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.alexmasson58.expensiblefabkotlin.ExpandableFab
-import com.alexmasson58.expensiblefabkotlin.GridExpendableFab
-import com.alexmasson58.expensiblefabkotlin.LinearExpandableFAB
+import com.alexmasson58.expensiblefabkotlin.Option
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ExpandableFab.Listener {
     override fun onOptionClicked(o: ExpandableFab.Option) {
         o.returnCode()
     }
 
-
-    @BindView(R.id.grid)
-    lateinit var grid: GridExpendableFab
-
-    @BindView(R.id.linear)
-    lateinit var linear: LinearExpandableFAB
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
-        listOf(grid, linear).forEach {
-            it.listener = this
+        listOf(linear).forEach {
+            //it.listener = this
             it.setOptions(
-                    ExpandableFab.Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
-                    ExpandableFab.Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() }),
-                    ExpandableFab.Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
-                    ExpandableFab.Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() }),
-                    ExpandableFab.Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
-                    ExpandableFab.Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() }),
                     ExpandableFab.Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
                     ExpandableFab.Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() }),
                     ExpandableFab.Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
                     ExpandableFab.Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() })
             )
-            it.invalidate()
+        }
+        listOf(flo).forEach {
+            //it.listener = this
+            it.setOptions(
+                    Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
+                    Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() }),
+                    Option(R.drawable.ic_baseline_add_24px, { Toast.makeText(this@MainActivity, "add", Toast.LENGTH_SHORT).show() }),
+                    Option(R.drawable.ic_baseline_edit_24px, { Toast.makeText(this@MainActivity, "edit", Toast.LENGTH_SHORT).show() })
+            )
         }
 
     }
---
+++ expensiblefabkotlin/src/main/java/com/alexmasson58/expensiblefabkotlin/Option.kt
@@ -0,0 +1,3 @@
+package com.alexmasson58.expensiblefabkotlin
+
+data class Option(var iconId: Int = -1, var returnCode: () -> Unit)
